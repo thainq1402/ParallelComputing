73ebbb1610a01ccc4961dd6a3d72bbe912478e31
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-11-27 18:08:53 +0700

    nghia

--- B. Thực hiện từng đề bài riêng/main.cpp
+++ B. Thực hiện từng đề bài riêng/main.cpp
@@ -1,60 +1,57 @@
 #include <iostream>
-// #include<iomanip>
 #include <ctime>
 #include <cstdlib>
-// #include <chrono>
+// #include<iomanip>
 #include <omp.h>
+#include <chrono>
 
-#define SIZE 10
+using namespace std;
+
+// #define SIZE 10
 // #define SIZE 100
 // #define SIZE 1000
 // #define SIZE 10000
 // #define SIZE 100000
-// #define SIZE 1000000
+#define SIZE 1000000
 // #define SIZE 10000000
 // #define SIZE 100000000
 // #define SIZE 1000000000
 
 double generateRandomNumber();                                           // Hàm sinh giá trị ngẫu nhiên
 double *createRandomVector(int size);                                    // Hàm tạo vector ngẫu nhiên
-double sequentialDotProduct(double *vectorA, double *vectorB, int size); // Hàm tuần tự tính tích vô hướng của 2 vector
-double parallelDotProduct(double *vectorA, double *vectorB, int size);   // Hàm song song tính tích vô hướng của 2 vector
-double parallelDotProduct_v1(double *vectorA, double *vectorB, int size);
+double calculatorEfficiency(double time_tt, double time_ss, int thread); // Hàm tính hiệu suất
+double sequentialDotProduct(double *vectorA, double *vectorB, int size); // Hàm tuần tự
+double parallelDotProduct(double *vectorA, double *vectorB, int size);   // Hàm song song
 
 int main()
 {
-    std::cout << "The sizes of the 2 vectors are: " << SIZE << std::endl;
+    double result, efficiency;
+    cout << "The sizes of the 2 vectors are: " << SIZE << endl;
 
+    // Tạo 2 vector ngẫu nhiên
     double *vectorA = createRandomVector(SIZE);
     double *vectorB = createRandomVector(SIZE);
 
     // Thực hiện tuần tự
-    double start_time_sequential =  omp_get_wtime();
-    double result_sequential = sequentialDotProduct(vectorA, vectorB, SIZE);
-    double end_time_sequential =  omp_get_wtime();
-    double duration_sequential =  (end_time_sequential - start_time_sequential);
-    std::cout << "Sequential programming" << std::endl;
-    std::cout << "The dot product of two vectors is: " << result_sequential << std::endl;
-    std::cout << "Program sequential execution time: " << duration_sequential << " seconds" << std::endl; // Tính thời gian thực thi
+    auto start_time_sequential = chrono::high_resolution_clock::now();
+    result = sequentialDotProduct(vectorA, vectorB, SIZE);
+    auto end_time_sequential = chrono::high_resolution_clock::now();
+    auto duration_sequential = chrono::duration_cast<std::chrono::microseconds>(end_time_sequential - start_time_sequential);
+    cout << "Sequential programming: result = " << result << ", time = " << duration_sequential.count() << " microseconds" << endl;
 
     // Thực hiện song song
-    double start_time_parallel =  omp_get_wtime();
-    long double result_parallel = parallelDotProduct(vectorA, vectorB, SIZE);
-    double end_time_parallel   =  omp_get_wtime();
-    double duration_parallel =  (end_time_parallel - start_time_parallel);
-    std::cout << "Parallel programming" << std::endl;
-    std::cout << "The dot product of two vectors is: " << result_parallel << std::endl;
-    std::cout << "Program parallel execution time: " <<   duration_parallel << " seconds" << std::endl; // Tính thời gian thực thi
-
-    
-    // Thực hiện song song
-    double start_time_parallel_v1 =  omp_get_wtime();
-    long double result_parallel_v1 = parallelDotProduct_v1(vectorA, vectorB, SIZE);
-    double end_time_parallel_v1   =  omp_get_wtime();
-    double duration_parallel_v1 =  (end_time_parallel_v1 - start_time_parallel_v1);
-    std::cout << "Parallel programming" << std::endl;
-    std::cout << "The dot product of two vectors is: " << result_parallel_v1 << std::endl;
-    std::cout << "Program parallel execution time: " << duration_parallel_v1   << " seconds" << std::endl; // Tính thời gian thực thi
+    int max_threads = omp_get_max_threads(); // Lấy số luồng tối đa theo từng máy
+    for (int thread = 2; thread < max_threads; thread++)
+    {
+        omp_set_num_threads(thread); // Lặp theo số luồng để lấy thời gian
+        auto start_time_parallel = chrono::high_resolution_clock::now();
+        result = parallelDotProduct(vectorA, vectorB, SIZE);
+        auto end_time_parallel = chrono::high_resolution_clock::now();
+        auto duration_parallel = chrono::duration_cast<std::chrono::microseconds>(end_time_parallel - start_time_parallel);
+        efficiency = calculatorEfficiency(duration_sequential.count(), duration_parallel.count(), thread);
+        cout << "Parallel: thread = " << thread << ", result = " << result << ", time = " << duration_parallel.count() << " microseconds"
+             << ", efficiency = " << efficiency << endl;
+    }
 
     // Giải phóng bộ nhớ
     delete[] vectorA;
@@ -66,7 +63,7 @@ double generateRandomNumber()
 {
     static bool seedInitialized = false;
 
-    // Nếu seed chưa được khởi tạo, hãy khởi tạo nó
+    // Kiểm tra khởi tạo seed
     if (!seedInitialized)
     {
         srand(time(NULL));
@@ -82,7 +79,7 @@ double *createRandomVector(int size)
 
     if (vector == nullptr)
     {
-        std::cerr << "Error: Failed to allocate memory." << std::endl;
+        cerr << "Error: Failed to allocate memory." << endl;
         exit(1);
     }
 
@@ -94,6 +91,10 @@ double *createRandomVector(int size)
     return vector;
 }
 
+double calculatorEfficiency(double time_tt, double time_ss, int thread)
+{
+    return (time_tt / time_ss) / thread;
+}
 double sequentialDotProduct(double *vectorA, double *vectorB, int size)
 {
     double result = 0.0;
@@ -111,53 +112,11 @@ double parallelDotProduct(double *vectorA, double *vectorB, int size)
 
     long double result = 0.0;
 
-#pragma omp parallel for reduction(+ : result)
+#pragma omp parallel for reduction(+ : result), schedule(static)
     for (int i = 0; i < size; ++i)
     {
-        // int threadNum = omp_get_thread_num();
-        // std::cout << "Thread " << threadNum << " is processing element " << i << std::endl;
         result += vectorA[i] * vectorB[i];
     }
 
     return result;
 }
-
-double parallelDotProduct_v1(double *vectorA, double *vectorB, int size)
-{
-
-    
-    long double result = 0.0;
-
-    #pragma omp parallel for reduction(+ : result), schedule(static)
-
-    for (int i = 0; i < size; ++i)
-    {
-        // int threadNum = omp_get_thread_num();
-        // std::cout << "Thread " << threadNum << " is processing element " << i << std::endl;
-        // int threadNum = omp_get_thread_num();
-        // std::cout << "Thread " << threadNum << " is processing element " << i << std::endl;
-        // int threadNum = omp_get_thread_num();
-        // std::cout << "Thread " << threadNum << " is processing element " << i << std::endl;
-        // int threadNum = omp_get_thread_num();
-        // std::cout << "Thread " << threadNum << " is processing element " << i << std::endl;
-        // int threadNum = omp_get_thread_num();
-        // std::cout << "Thread " << threadNum << " is processing element " << i << std::endl;
-        result += vectorA[i] * vectorB[i];
-    }
-    return result;
-}
-double parallelDotProduct_v2(double *vectorA, double *vectorB, int size)
-{
-
-    
-    long double result = 0.0;
-
-    #pragma omp parallel for reduction(+ : result), schedule(static)
-
-    for (int i = 0; i < size; ++i)
-    {
-        result += vectorA[i] * vectorB[i];
-    }
-    return result;
-}
-
