40e81f2baf502394278211a83f7cf99d4d94f1e0
thainq1402 <nguyenquocthaihn142@gmail.com>
2023-11-30 10:27:24 +0700

    sửa kiểu dữ liệu cho biến 'result'

--- B. Thực hiện từng đề bài riêng/main.cpp
+++ B. Thực hiện từng đề bài riêng/main.cpp
@@ -109,7 +109,7 @@ void releaseMemory(double *vectorA, double *vectorB)
 }
 double sequentialDotProduct(double *vectorA, double *vectorB, int size)
 {
-    double result = 0.0;
+    long double result = 0.0;
 
     for (int i = 0; i < size; i++)
     {
